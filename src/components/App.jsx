4ccf64f08ddf2fd6b60e38b0106ed310f54a55f7
Christophe Pollet <cpollet@users.noreply.github.com>
2015-11-23 21:27:34 +0100

    added react's material-ui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,39 @@
+'use strict';
+
 import React from 'react';
+import MaterialUI from 'material-ui';
+import Theme from '../theme.js';
+
+var ThemeManager = MaterialUI.Styles.ThemeManager;
+var AppBar = MaterialUI.AppBar;
 
 class App extends React.Component {
     constructor() {
         super();
+
         this.state = {
             message: 'Hello from reactJS'
         };
     }
 
+    getChildContext() {
+        return {
+            muiTheme: ThemeManager.getMuiTheme(Theme)
+        }
+    }
+
     render() {
         return (
-           <b>{this.state.message}</b>
+            <div>
+                <AppBar title="My Timesheets"/>
+                <b>{this.state.message}</b>
+            </div>
         );
     }
 }
+
+App.childContextTypes = {
+    muiTheme: React.PropTypes.object
+};
 
 export default App;
